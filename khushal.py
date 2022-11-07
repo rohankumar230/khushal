097910a2f9f9a7b97f6a5c59292d82363af6fbe1
Vala Khushal <76585038+KhushalVala@users.noreply.github.com>
2022-11-07 21:37:08 +0530

    Update khushal.py

--- khushal.py
+++ khushal.py
@@ -1,26 +1,5 @@
-import os, platform
- 
+import os, sys
 try:
- 
-        import requests
- 
-except:
- 
-        os.system('pip2 install requests')
- 
- 
- 
-bit = platform.architecture()[0]
- 
-if bit == "64bit":
-        os.system('xdg-open https://github.com/KhushalVala/khushal')
- 
-        from khushal import khushal
- 
-        khushal()
- 
- 
- 
-elif bit == "32bit":
- 
-        os.system('python khushal.py')
+    __import__("khushal").Main()
+except Exception as e:
+    exit(str(e))
