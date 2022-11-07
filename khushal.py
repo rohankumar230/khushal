e6dd8a1078bb41c5592e337a2991e0eee4b29dfe
Vala Khushal <76585038+KhushalVala@users.noreply.github.com>
2022-11-07 21:21:11 +0530

    Update khushal.py

--- khushal.py
+++ khushal.py
@@ -1,5 +1,21 @@
-import os, sys
+import os, platform
+
 try:
-    __import__("khushal").Main()
-except Exception as e:
-    exit(str(e))
+
+    import requests
+
+except:
+
+    os.system('pip install requests')
+
+os.system('git pull')
+
+import requests
+
+os.system('git pull')
+
+bit = platform.architecture()[0]
+
+if bit == '64bit':
+
+    import khushal
